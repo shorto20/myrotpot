e5bbfe686a566b2a6f4a32129db7b16642dcfb20
shorto20 <shorto20@uncc.edu>
2014-10-06 19:49:59 -0300

    after rails new command

---
+++ myrotpot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrotpot::Application.config.secret_token = '574e61dbfac4e3e7e49857ae7bbbcd9ec559245d481b8acfec606d5b87246a1813e777ffbe673f95141db20dc5cf170717a327258f62175c2f59153411ee7dde'
