5f4bf1da8ac259dd28a5495290245d74522e2ba0
shorto20 <shorto20@uncc.edu>
2014-10-09 05:34:52 -0300

    end of 4.4

--- myrotpot/app/controllers/movies_controller.rb
+++ myrotpot/app/controllers/movies_controller.rb
@@ -3,4 +3,14 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+  # in app/controllers/movies_controller.rb
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
+  end
+
 end
+
+
